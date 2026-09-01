4e01c478f75febdfafcd5691f28ed603eb0bfdc1
Fiandriananaprime <hei.fiandrianana@gmail.com>
2026-09-01 18:24:42 +0300

    feat(register): add register api

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,7 +1,23 @@
 import { baseApi } from "./axios";
-import { LoginRequest, AuthResponse } from "../type/auth";
+import { 
+    LoginRequest, 
+    AuthResponse ,
+    RegisterRequest,
+    RegisterSellerRequest,
+    RegisterSellerResponse
+  } from "../type/auth";
 
 export const login = async (credentials: LoginRequest): Promise<AuthResponse> => {
   const response = await baseApi.post<AuthResponse>("/auth/login", credentials);
+  return response.data;
+}
+
+export const register = async (data: RegisterRequest): Promise<AuthResponse> => {
+  const response = await baseApi.post<AuthResponse>("/auth/register", data);
+  return response.data;
+}
+
+export const registerSeller = async (data: RegisterSellerRequest): Promise<RegisterSellerResponse> => {
+  const response = await baseApi.post<RegisterSellerResponse>("/auth/register/seller", data);
   return response.data;
 }
--- src/type/auth.ts
+++ src/type/auth.ts
@@ -1,5 +1,7 @@
 import { User } from './user';
 
+export type status = "pending" | "approved" | "rejected";
+
 export interface LoginRequest {
   email: string;
   password: string;
@@ -15,4 +17,29 @@ export interface AuthTokens {
 export interface AuthResponse {
   token: AuthTokens;
   user: User;
+}
+
+export interface RegisterRequest {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
+
+export interface RegisterSellerRequest extends RegisterRequest {
+   businessName: string;
+   phone: string;
+   location: string;
+}
+
+export interface RegisterSellerResponse {
+    id:String;
+    businessName: String;
+    owner: String;
+    email: String;
+    phone: String;
+    location: String;
+    category: String;
+    date: String;
+    status: status;
 }
